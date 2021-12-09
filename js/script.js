c18ca2bfc6206b9e4f2bf38942d784d39589faf1
ElisaSalvatore <elisa.salvatore110@gmail.it>
2021-12-09 10:19:10 +0100

    document fixed parseInt added

--- js/script.js
+++ js/script.js
@@ -1,39 +1,43 @@
-// variabile collegata al file html
+// Variabile collegata al file html
 const outputHtml = document.getElementById("output");
 
-// chiedere i km della tratta
-let kilometers = prompt("Inserisci il numero di km da percorrere:");
-console.log(kilometers);
-// chiedere l'età del passeggero
-let age = prompt("Inserisci la tua età:");
-console.log(age);
+// Constante applicativo, fissato in alto è più facile da modificare in futuro.
+const pricePerkm = 0.21; 
+const discount18 = 20;
+const discount65 = 40;
+
+// Chiedere i km della tratta
+const km = parseInt(prompt("Inserisci il numero di km da percorrere:"));
+// Chiedere l'età del passeggero
+const age = parseInt(prompt("Inserisci la tua età:"));
 
 
 // Prezzo del biglietto senza sconto
-const kmPrice = 0.21;
-let ticketPrice = kilometers * kmPrice;
-console.log(ticketPrice);
+let price = pricePerkm * km;
+console.log(price);
 
 
-// Prezzo del biglietto scontato
-let discountValue;
-if (age < 18 && age >= 1) {
-    discountValue = ticketPrice * 0.8;
+// Prezzo del biglietto scontato (20% -> 0.8 || 40% -> 0.6)
+if (age < 18) {
+    const discount = price * discount18 / 100;
+    price = price - discount;
     outputHtml.innerHTML = "</br> È stato applicato uno sconto del 20% sul prezzo totale!";  
-} else if (age > 65) {
-    discountValue = ticketPrice * 0.6; 
+}
+
+if (age >= 65) {
+    const discount = price * discount65 / 100;
+    price = price - discount;
     outputHtml.innerHTML += "</br> È stato applicato uno sconto del 40% sul prezzo totale!"; 
 } else {
-    discountValue = ticketPrice;
+    discount = price;
     outputHtml.innerHTML += "</br> NON è stato applicato alcuno sconto sul prezzo totale.";  
 }
 
-outputHtml.innerHTML += `<br/>Il costo totale del biglietto è di ${discountValue}€.`;
-console.log(discountValue);
-
+console.log(price);
 
 // Mostrare solamente due cifre decimali per specificare il costo del biglietto
-discountValue = discountValue.toFixed(2);
+price = parseFloat(price.toFixed(2));
+console.log(price);
 
-// Per arrotondare all'intero più vicino, per difetto o per eccesso: 2.45 = 2 | 2.60 = 3
-/* let rounded = Math.round((discountValue + ) * 100) / 100 */
+outputHtml.innerHTML += `<br/>Il costo totale del biglietto è di ${price}€.`;
+
